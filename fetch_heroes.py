b30c340513ff8cac8c4725ce1a732740504e746b
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2023-03-18 19:16:56 -0400

    feat: initialize python Avengers project

---
+++ fetch_heroes.py
@@ -0,0 +1,22 @@
+
+import requests
+import json
+import hashlib
+import datetime
+from random import randint
+# import pyodbc
+from marvelClass import Marvel
+
+# Define API endpoint and authentication keys
+# optimization: use environment variables
+
+public_key = "dda446b2e1c24ad15b95764453ea581f"
+private_key = "981b3c906e07a0d1f4acf631bf103087b13099c6"
+x = Marvel(public_key, private_key)
+
+row = x.fetch_characters()
+print("Comic Character: ", row)
+
+# write to a file
+with open('marvel.json', 'w') as f:
+    json.dump(row, f)
---
+++ marvelClass.py
@@ -0,0 +1,52 @@
+
+import requests
+import hashlib
+import datetime
+from random import randint
+import json
+from collections import Counter
+
+
+class Marvel():
+
+    def __init__(self, public_key, private_key):
+        # private_key and public_key are used to generate an md5 hash string
+        self.private_key = private_key
+        self.public_key = public_key
+        self.baseURI = "http://gateway.marvel.com/v1/public"
+        # self.hashed_String,self.timestamp = self.md5_Hash()
+        self.hashed_String = self.md5_Hash()
+        self.hash_input = self.md5_Hash()
+        self.url_params = {
+            'ts': self.timestamp,
+            'apikey': self.public_key,
+            'hash': self.hashed_String,
+        }
+
+    def fetch_characters(self):
+        self.baseURI += "/characters?name=Wolverine"
+        # print(self.baseURI)
+        resp = requests.get(self.baseURI, params=self.url_params)
+        return resp.json()
+
+    def fetch_characters_by_id(self, char_id="1009652", limit=20):
+        # limit specifies how  many items to display
+        if limit is not None:
+            self.url_params['limit'] = limit
+        self.char_id = char_id
+        # add the item in the param payload
+        self.baseURI += "/characters" + "/{}".format(self.char_id)
+        resp = requests.get(self.baseURI, params=self.url_params)
+        # print(resp.url)
+        return resp.json()
+
+    def md5_Hash(self):
+        '''
+        hash changes everytime this function is called.
+        returns hashed string and a timestamp
+        '''
+        self.timestamp = '{:%Y%m%d%H%M%S}'.format(
+            datetime.datetime.now())  # generate a timestamp
+        self.hash_input = self.timestamp + self.private_key + self.public_key
+        self.hashed_String = hashlib.md5(self.hash_input.encode(
+            'utf-8')).hexdigest()  # generate md5 hash
