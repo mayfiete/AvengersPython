847555b3465f6aa964deba9cdf33a985cce5e66e
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2023-03-23 13:21:27 -0400

    update stored procedure example

--- sql/usp_character_comic_count.sql
+++ sql/usp_character_comic_count.sql
@@ -1,42 +1,10 @@
-
-
-create table character_comic (
-	id serial, 
-	name varchar(250), 
-	comic varchar(750), 
-	character_id bigint
-); 
-
-select * 
-from character_comic; 
-
-alter table character_comic 
-	add character_id bigint; 
-
--- truncate table character_comic; 
-
-
-
-select * 
-from pg_user 
-; 
-
-
 create table hero_count ( 
 	hero varchar, 
 	count integer 
 ); 
 
-select * 
-from hero_count 
-; 
 
-
-
-call usp_character_comic_count() ; 
 -- drop procedure usp_character_comic_count; 
-
-
 CREATE PROCEDURE usp_character_comic_count()
   LANGUAGE plpgsql
 AS $$
@@ -50,3 +18,9 @@ begin
               
 END;
 $$;
+
+call usp_character_comic_count() ; 
+
+select * 
+from hero_count 
+; 
