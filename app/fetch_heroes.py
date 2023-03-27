6c6735a213765423f55a6ab5846922a72177d303
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2023-03-27 15:46:03 -0400

    feat: updating comments related to fetch_heroes.py script

--- app/fetch_heroes.py
+++ app/fetch_heroes.py
@@ -6,6 +6,9 @@ import datetime
 import os
 from MarvelClass import Marvel
 
+# In real life, you would not store your keys in the code.
+# Instead, you would store them in a configuration file or environment variables.
+
 # Define API endpoint and authentication keys
 # optimization: use environment variables
 
@@ -28,3 +31,8 @@ print("Comic Character: ", row)
 # write to a file
 with open('marvel.json', 'w') as f:
     json.dump(row, f)
+
+
+# the following variables can be passed to the fetch_heroes function
+# export MARVEL_PUBLIC_KEY="dda446b2e1c24ad15b95764453ea581f"
+# export MARVEL_PRIVATE_KEY="981b3c906e07a0d1f4acf631bf103087b13099c6"
