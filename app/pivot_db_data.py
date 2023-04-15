38853276c9b70da27565ed69640574eaa2003884
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2023-04-15 18:38:18 -0400

    feat: update bar chart visualization

--- app/pivot_db_data.py
+++ app/pivot_db_data.py
@@ -1,9 +1,10 @@
-
-# create a class to encapsulate the database connection
-from matplotlib import pyplot as plt
+# create a class to encapsulate the database connection and query execution
+# from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 import csv
 import pandas as pd
 import numpy as np
+import seaborn as sns
 from DbConnx import DbConnx
 
 conn = DbConnx()
@@ -24,11 +25,24 @@ df = pd.DataFrame(rows, columns=['name', 'comic'])
 # count the number of comics for each character
 comic_counts = df.pivot_table(index='name', aggfunc='count')
 
-print(comic_counts)
+# Set the color palette
+sns.set_palette("muted")
 
 # create a bar chart image of the data
-comic_counts.plot(
-    kind='bar', title='Number of Comics per Character', legend=True)
+ax = comic_counts.plot(
+    kind='bar', title='Number of Comics per Character', legend=False)
+
+# Add labels to the x-axis and y-axis
+ax.set_xlabel('Character')
+ax.set_ylabel('Number of Comics')
+
+# Add a grid
+ax.grid(axis='y')
+
+# Rotate the x-axis labels
+plt.xticks(rotation=45, ha='right')
+
+plt.tight_layout()
 plt.savefig('comic_counts.png')
 
 # close the cursor and connection to so the server can allocate
