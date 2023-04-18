ba33226a2b30d318dea9a954c0a780961a55f9f7
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2023-04-18 14:13:50 -0400

    feat: refactor to use class for SetAnalysis operations

---
+++ app/SetAnalysisClass.py
@@ -0,0 +1,20 @@
+# if you like a hero, find the other heroes that share the same comic books
+from SuperheroClass import Superhero as sh
+
+
+# create a class to look at the intersection of two sets
+class SetAnalysis:
+    def __init__(self, hero1=None, hero2=None):
+        self.hero1 = hero1 or sh('Rogue', ['X-Men', 'Avengers'])
+        self.hero2 = hero2 or sh('Wolverine', ['X-Men', 'X-Force'])
+
+    def intersection(self):
+        return self.hero1.comics.intersection(self.hero2.comics)
+
+
+x = SetAnalysis().intersection()
+print(x)
+
+
+y = SetAnalysis(sh('Rogue'), sh('Wolverine')).intersection()
+print(y)
--- app/set_analysis.py
+++
@@ -1,9 +0,0 @@
-# if you like a hero, find the other heroes that share the same comic books
-from SuperheroClass import Superhero as sh
-
-hero1 = sh('Rogue')
-hero2 = sh('Wolverine')
-
-shared_comics = hero1.comics.intersection(hero2.comics)
-
-print(shared_comics)
