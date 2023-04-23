b7cf21785473d0a5fa75face423855165b90d6f8
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2023-04-22 21:32:45 -0400

    feat: find comics from 2017

--- app/NextComicIterable.py
+++ app/NextComicIterable.py
@@ -4,6 +4,8 @@ from NextComicToRead import NextComicToRead
 
 # Simply put, an iterator is an object that implements the iterator protocol,
 # similar to how a sequence implements the sequence protocol.
+
+
 class NextComicIterable(Superhero):
     def __init__(self, name, comics=[]):
         self.name = name
@@ -22,3 +24,8 @@ nci = NextComicIterable(name="Wolverine")
 # iterate over the comics in the NextComicIterable object
 for comic in nci:
     print(comic)
+
+# find comics from 2017
+for comic in nci:
+    filtered_strings = list(filter(lambda x: '2017' in x, comic))
+    print(filtered_strings)
